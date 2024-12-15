e35bdccfa2d852be86a89946b4305677de704654
Rodrigo Hashimoto <rodhash@gmail.com>
2024-12-15 00:14:45 -0300

    Initial commit
    Signed-off-by: Rodrigo Hashimoto <rodhash@gmail.com>

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rodhash/goPretty
+
+go 1.23.4
---
+++ prettyPrint.go
@@ -0,0 +1,29 @@
+
+package gopretty
+
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+)
+
+type mapObj map[float64][]float64
+
+// Takes a MAP object and pretty Print it
+func goPretty(data mapObj) {
+	convertedData := make(map[string]any)
+
+	for k,v := range data {
+		convertedData[strconv.FormatFloat(k, 'f', -1, 64)] = v
+	}
+
+	jsonData, err := json.MarshalIndent(convertedData, "", "  ")
+
+	if err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+
+	fmt.Println(string(jsonData))
+}
+
